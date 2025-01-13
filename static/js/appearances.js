5e8021e3300e3287071b56f6bc6521e530b423d5
themizzi <themizzi@me.com>
2025-01-12 19:10:22 -0600

    chore: fix more js lint errors

--- static/js/appearances.js
+++ static/js/appearances.js
@@ -1,18 +1,18 @@
 function openTab(evt, cityName) {
-  var i, tabcontent, tablinks
-  tabcontent = document.getElementsByClassName('tab-content')
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName('tab-content');
   for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = 'none'
+    tabcontent[i].style.display = 'none';
   }
-  tablinks = document.getElementsByClassName('tab-button')
+  tablinks = document.getElementsByClassName('tab-button');
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(' active', '')
+    tablinks[i].className = tablinks[i].className.replace(' active', '');
   }
-  document.getElementById(cityName).style.display = 'block'
-  evt.currentTarget.className += ' active'
+  document.getElementById(cityName).style.display = 'block';
+  evt.currentTarget.className += ' active';
 }
 
 // Call this function to default to the first tab
 document.addEventListener('DOMContentLoaded', (event) => {
-  document.querySelector('.tab-button').click()
-})
+  document.querySelector('.tab-button').click();
+});
--- static/js/newsletter.js
+++ static/js/newsletter.js
@@ -1,12 +1,12 @@
 (function (s, e, n, d, er) {
-  s['Sender'] = er
+  s['Sender'] = er;
   s[er] = s[er] || function () {
-    (s[er].q = s[er].q || []).push(arguments)
-  }, s[er].l = 1 * new Date()
+    (s[er].q = s[er].q || []).push(arguments);
+  }, s[er].l = 1 * new Date();
   var a = e.createElement(n),
-    m = e.getElementsByTagName(n)[0]
-  a.async = 1
-  a.src = d
-  m.parentNode.insertBefore(a, m)
-})(window, document, 'script', 'https://cdn.sender.net/accounts_resources/universal.js', 'sender')
-sender('c109cedd4d8953')
+    m = e.getElementsByTagName(n)[0];
+  a.async = 1;
+  a.src = d;
+  m.parentNode.insertBefore(a, m);
+})(window, document, 'script', 'https://cdn.sender.net/accounts_resources/universal.js', 'sender');
+sender('c109cedd4d8953');
