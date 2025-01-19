f3f2a85fa129bdce8f461ec44152b0085b13a509
themizzi <themizzi@me.com>
2025-01-19 18:44:24 +0000

    chore: migrate to typescript

---
+++ assets/scripts/theme-toggle.ts
@@ -0,0 +1,51 @@
+export class ThemeToggle {
+  constructor(
+    private themeColorMeta: HTMLElement,
+    private themeIconSystem: HTMLElement,
+    private themeIconLight: HTMLElement,
+    private themeIconDark: HTMLElement,
+    private documentElement: HTMLElement,
+  ) {
+    this.initializeTheme();
+    this.addEventListeners();
+  }
+
+  private setThemeColor(): void {
+    const backgroundColor = getComputedStyle(this.documentElement).getPropertyValue('--background-color').trim();
+    this.themeColorMeta.setAttribute('content', backgroundColor);
+  }
+
+  private setTheme(theme: string): void {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+
+    // Hide all icons initially
+    this.themeIconSystem.style.display = 'none';
+    this.themeIconLight.style.display = 'none';
+    this.themeIconDark.style.display = 'none';
+
+    // Show the correct icon based on the selected theme
+    if (theme === 'light' && this.themeIconLight) {
+      this.themeIconLight.style.display = 'inline';
+    }
+    else if (theme === 'dark' && this.themeIconDark) {
+      this.themeIconDark.style.display = 'inline';
+    }
+    else if (this.themeIconSystem) {
+      this.themeIconSystem.style.display = 'inline';
+    }
+
+    this.setThemeColor();
+  }
+
+  private initializeTheme(): void {
+    const savedTheme = localStorage.getItem('theme') || 'system';
+    this.setTheme(savedTheme);
+  }
+
+  private addEventListeners(): void {
+    this.themeIconSystem.addEventListener('click', () => this.setTheme('light'));
+    this.themeIconLight.addEventListener('click', () => this.setTheme('dark'));
+    this.themeIconDark.addEventListener('click', () => this.setTheme('system'));
+  }
+}
