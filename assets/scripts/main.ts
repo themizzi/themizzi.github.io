690b18a3268d6ef7beecbf724a20aad2b9efd8db
themizzi <themizzi@me.com>
2025-01-20 01:56:27 +0000

    feat: add social partial and socials to contact page

--- assets/scripts/main.ts
+++ assets/scripts/main.ts
@@ -23,9 +23,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
 document.addEventListener('DOMContentLoaded', function () {
   // show all elements with .javascript-enable class
-  const elements = document.querySelectorAll('.javascript-enable');
-  elements.forEach((element) => {
+  document.querySelectorAll('.javascript-enable').forEach((element) => {
     // remove the hidden attribute
     element.removeAttribute('hidden');
+  });
+
+  // hide all elements with .javascript-disable class
+  document.querySelectorAll('.javascript-disable').forEach((element) => {
+    // add the hidden attribute
+    element.setAttribute('hidden', '');
   });
 });
