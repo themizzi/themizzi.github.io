d2cfee3c28103d086da28a84bbc9056b3e394c8d
themizzi <themizzi@me.com>
2025-02-01 03:47:43 +0000

    refactor: theme selector does not require javascript

--- assets/scripts/main.ts
+++ assets/scripts/main.ts
@@ -3,16 +3,18 @@ import { ThemeToggle } from './theme-toggle';
 // Initialize the ThemeToggle class with the necessary elements
 document.addEventListener('DOMContentLoaded', () => {
   const themeColorMeta = document.getElementById('theme-color-meta') as HTMLElement;
-  const themeIconSystem = document.getElementById('theme-icon-system') as HTMLElement;
-  const themeIconLight = document.getElementById('theme-icon-light') as HTMLElement;
-  const themeIconDark = document.getElementById('theme-icon-dark') as HTMLElement;
+  const themeIconSystem = document.getElementById('theme-system') as HTMLElement;
+  const themeIconLight = document.getElementById('theme-light') as HTMLElement;
+  const themeIconDark = document.getElementById('theme-dark') as HTMLElement;
 
   if (themeColorMeta && themeIconSystem && themeIconLight && themeIconDark) {
     new ThemeToggle(
       themeColorMeta,
-      themeIconSystem,
-      themeIconLight,
-      themeIconDark,
+      [
+        themeIconSystem,
+        themeIconLight,
+        themeIconDark,
+      ],
       document.documentElement,
     );
   }
--- assets/scripts/theme-toggle.ts
+++ assets/scripts/theme-toggle.ts
@@ -1,42 +1,41 @@
 export class ThemeToggle {
   constructor(
     private themeColorMeta: HTMLElement,
-    private themeIconSystem: HTMLElement,
-    private themeIconLight: HTMLElement,
-    private themeIconDark: HTMLElement,
+    private themeToggles: HTMLElement[],
     private documentElement: HTMLElement,
   ) {
-    this.setTheme(localStorage.getItem('theme') || 'system');
-    this.themeIconSystem.addEventListener('click', () => this.setTheme('light'));
-    this.themeIconLight.addEventListener('click', () => this.setTheme('dark'));
-    this.themeIconDark.addEventListener('click', () => this.setTheme('system'));
+    // load theme from localStorage
+    const theme = localStorage.getItem('theme') || 'system';
+
+    // initialize toggles
+    this.themeToggles.forEach((toggle) => {
+      const value = toggle.getAttribute('value');
+      if (value === theme) {
+        toggle.setAttribute('checked', '');
+      }
+      else {
+        toggle.removeAttribute('checked');
+      }
+    });
+
+    this.setTheme();
+
+    // add event listeners
+    this.themeToggles.forEach((toggle) => {
+      toggle.addEventListener('click', () => {
+        const theme = toggle.getAttribute('value') || 'system';
+        if (theme === 'system') {
+          localStorage.removeItem('theme');
+        }
+        else {
+          localStorage.setItem('theme', theme);
+        }
+        this.setTheme();
+      });
+    });
   }
 
-  private setTheme(theme: string): void {
-    if (theme !== 'system') {
-      document.documentElement.setAttribute('data-theme', theme);
-    }
-    else {
-      document.documentElement.removeAttribute('data-theme');
-    }
-    localStorage.setItem('theme', theme);
-
-    // Hide all icons initially
-    this.themeIconSystem.style.display = 'none';
-    this.themeIconLight.style.display = 'none';
-    this.themeIconDark.style.display = 'none';
-
-    // Show the correct icon based on the selected theme
-    if (theme === 'light' && this.themeIconLight) {
-      this.themeIconLight.style.display = 'inline';
-    }
-    else if (theme === 'dark' && this.themeIconDark) {
-      this.themeIconDark.style.display = 'inline';
-    }
-    else if (this.themeIconSystem) {
-      this.themeIconSystem.style.display = 'inline';
-    }
-
+  private setTheme(): void {
     const backgroundColor = getComputedStyle(this.documentElement).getPropertyValue('--background-color').trim();
     this.themeColorMeta.setAttribute('content', backgroundColor);
   }
